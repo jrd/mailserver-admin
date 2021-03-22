402f3fbf96a0d7aba202aea64f0609d7980fb907
Cyrille Pontvieux <cyrille@enialis.net>
2021-03-22 17:35:39 +0100

    domain create/edit views done

--- mailserveradmin/static/mailserveradmin/js/app.js
+++ mailserveradmin/static/mailserveradmin/js/app.js
@@ -1,33 +1,70 @@
 window.addEventListener('load', (event) => {
-    initSidebarToggleEvent();
+  initSidebarToggleEvent();
 });
 
 function initSidebarToggleEvent() {
-    const body = document.body;
-    const cssClassName = 'sidebar-visible';
-    let glassPane;
-    const navbtn = document.getElementById('navigation-toggler');
-    const navbtn2 = document.getElementById('navigation-toggler2');
-    const togglerFunction = function() {
-        body.classList.toggle(cssClassName);
-        if (body.classList.contains(cssClassName)) {
-            glassPane = document.createElement('div');
-            glassPane.classList.add('glass-pane', 'fade', 'show');
-            glassPane.onclick = function() {
-                body.classList.remove(cssClassName);
-                body.removeChild(glassPane);
-                glassPane = null;
-            };
-            body.appendChild(glassPane);
-        } else {
-            body.removeChild(glassPane);
-            glassPane = null;
-        }
-    };
-    if (navbtn) {
-        navbtn.addEventListener('click', togglerFunction);
+  const body = document.body;
+  const cssClassName = 'sidebar-visible';
+  let glassPane;
+  const navbtn = document.getElementById('navigation-toggler');
+  const navbtn2 = document.getElementById('navigation-toggler2');
+  const togglerFunction = function() {
+    body.classList.toggle(cssClassName);
+    if (body.classList.contains(cssClassName)) {
+      glassPane = document.createElement('div');
+      glassPane.classList.add('glass-pane', 'fade', 'show');
+      glassPane.onclick = function() {
+        body.classList.remove(cssClassName);
+        body.removeChild(glassPane);
+        glassPane = null;
+      };
+      body.appendChild(glassPane);
+    } else {
+      body.removeChild(glassPane);
+      glassPane = null;
     }
-    if (navbtn2) {
-        navbtn2.addEventListener('click', togglerFunction);
-    }
+  };
+  if (navbtn) {
+    navbtn.addEventListener('click', togglerFunction);
+  }
+  if (navbtn2) {
+    navbtn2.addEventListener('click', togglerFunction);
+  }
+}
+
+function getNewPrivateKey(privateKeyElementId) {
+  const privateKeyElement = document.getElementById(privateKeyElementId);
+  fetch('/get_new_private_key')
+    .then(response => response.json())
+    .then(data => {
+      privateKeyElement.value = data.private_key_pem;
+      privateKeyElement.dispatchEvent(new Event('change'));
+    });
+}
+
+function getDnsRecord(selectorValue, privateKeyValue, dnsRecordElementId) {
+  const dnsRecordElement = document.getElementById(dnsRecordElementId);
+  fetch('/get_dkim_dns_record', {
+    method: 'POST',
+    body: JSON.stringify({
+      selector: selectorValue,
+      private_key_pem: privateKeyValue,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      dnsRecordElement.innerHTML = data.dns_record || '';
+    });
+}
+
+function listenForSelectorOrPrivateKeyChange(selectorElementId, privateKeyElementId, dnsRecordElementId) {
+  const selectorElement = document.getElementById(selectorElementId);
+  const privateKeyElement = document.getElementById(privateKeyElementId);
+  const updateDnsRecordFunction = function(event) {
+    const selectorElement = document.getElementById(selectorElementId);
+    const privateKeyElement = document.getElementById(privateKeyElementId);
+    getDnsRecord(selectorElement.value, privateKeyElement.value, dnsRecordElementId);
+  };
+  selectorElement.addEventListener('change', updateDnsRecordFunction);
+  privateKeyElement.addEventListener('change', updateDnsRecordFunction);
 }
--- mailserveradmin/views.py
+++ mailserveradmin/views.py
@@ -1,13 +1,23 @@
 from datetime import date
+from json import loads
+from math import ceil
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 from Crypto.PublicKey import RSA
 from django.contrib.auth.mixins import LoginRequiredMixin as OrigLoginRequiredMixin
 from django.contrib.auth.views import LoginView as OrigLoginView
 from django.contrib.auth.views import LogoutView  # noqa F401
-from django.urls import reverse, reverse_lazy
+from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.urls import (
+    reverse,
+    reverse_lazy,
+)
 from django.views.generic import (
     DetailView,
     ListView,
+    View,
 )
 from django.views.generic.base import ContextMixin
 from django.views.generic.edit import (
@@ -48,8 +58,37 @@ class LoginRequiredMixin(OrigLoginRequiredMixin):
     login_url = reverse_lazy(f'{app_name}:login')
 
 
-def index_view(request):
-    return redirect(reverse(f'{app_name}:domain-list'))
+class IndexView(View):
+    def get(self, request):
+        return redirect(reverse(f'{app_name}:domain-list'))
+
+
+class NewPrivateKeyView(View):
+    def get(self, request):
+        private_key_pem = RSA.generate(2048).export_key().decode('ascii')
+        return JsonResponse({'private_key_pem': private_key_pem})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DkimDnsRecordView(View):
+    @classmethod
+    def get_dns_record(cls, selector, private_key_pem):
+        dns_record = ''
+        if selector and private_key_pem:
+            priv_key = RSA.import_key(private_key_pem)
+            pub_key = ''.join(priv_key.public_key().export_key().decode('ascii').split('\n')[1:-1])
+            dns_record = f'{selector}._domainkey 300 IN TXT "v=DKIM1; h=sha256; t=s; p='
+            parts = 40
+            dns_record += '"\n"'.join(
+                pub_key[i * parts:(i + 1) * parts] for i in range(ceil(len(pub_key) / parts))
+            ) + '"'
+        return dns_record
+
+    def post(self, request):
+        req = loads(request.body.decode('utf8'))
+        selector = req.get('selector', '')
+        private_key_pem = req.get('private_key_pem', '')
+        return JsonResponse({'dns_record': self.get_dns_record(selector, private_key_pem)})
 
 
 # ##############
@@ -87,19 +126,33 @@ class DomainCreateView(DomainContextMixin, LoginRequiredMixin, CreateView):
     success_url = reverse_lazy(f'{app_name}:domain-list')
     fields = ['name', 'dkim_enabled', 'dkim_selector', 'dkim_private_key']
 
+    def get_form(self):
+        form = super().get_form()
+        form.fields['dkim_private_key'].widget.attrs['cols'] = 80
+        form.fields['dkim_private_key'].widget.attrs['rows'] = 15
+        return form
+
     def get_initial(self):
+        selector = str(date.today().year)
+        private_key_pem = RSA.generate(2048).export_key().decode('ascii')
         return {
             'dkim_enabled': True,
-            'dkim_selector': str(date.today().year),
-            'dkim_private_key': RSA.generate(2048).export_key().decode('ascii'),
+            'dkim_selector': selector,
+            'dkim_private_key': private_key_pem,
+        }
+
+    def get_context_data(self):
+        ctx = super().get_context_data()
+        selector = ctx['form'].initial['dkim_selector']
+        private_key_pem = ctx['form'].initial['dkim_private_key']
+        dns_record = DkimDnsRecordView.get_dns_record(selector, private_key_pem)
+        return ctx | {
+            'dns_record': dns_record,
         }
 
 
-class DomainUpdateView(DomainContextMixin, LoginRequiredMixin, UpdateView):
-    model = MailDomain
+class DomainUpdateView(DomainCreateView, UpdateView):
     template_name_suffix = '_edit'
-    success_url = reverse_lazy(f'{app_name}:domain-list')
-    fields = ['name', 'dkim_enabled', 'dkim_selector', 'dkim_private_key']
 
 
 class DomainDeleteView(DomainContextMixin, LoginRequiredMixin, DeleteView):
